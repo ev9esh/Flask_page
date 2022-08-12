9f049ced3131ee75a62108916de12e6b5db16d90
User <ev9esh.mail@gmail.com>
2022-08-12 15:48:44 +0300

    add db

--- main.py
+++ main.py
@@ -1,6 +1,22 @@
 from flask import Flask, render_template, request
+from flask_sqlalchemy import SQLAlchemy
+from datetime import datetime
 
 app = Flask(__name__)
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///blog.db'
+db = SQLAlchemy(app)
+
+
+class Article(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    title = db.Column(db.String(100), nulllable=False)
+    intro = db.Column(db.String(300), nulllable=False)
+    text = db.Column(db.Text, nulllable=False)
+    created_on = db.Column(db.DateTime, default=datetime.utcnow)
+    updated_on = db.Column(db.DateTime, default=datetime.utcnow, onupdate=datetime.utcnow)
+
+    def __repr__(self):
+        return '<Article %r' % self.id
 
 
 @app.route('/')
