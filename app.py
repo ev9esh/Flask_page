f9a16be5d370004392d5db932bb26de6dda73d5f
User <ev9esh.mail@gmail.com>
2022-08-18 17:38:56 +0300

    fix update

--- app.py
+++ app.py
@@ -57,15 +57,13 @@ def posts_delete(id):
 
 @app.route('/posts/<int:id>/update', methods=['POST', 'GET'])
 def post_update(id):
+    article = Article.query.get(id)
     if request.method == 'POST':
-        title = request.form['title']
-        intro = request.form['intro']
-        text = request.form['text']
-
-        article = Article(title=title, intro=intro, text=text)
+        article.title = request.form['title']
+        article.intro = request.form['intro']
+        article.text = request.form['text']
 
         try:
-            db.session.add(article)
             db.session.commit()
             return redirect('/posts')
         except:
